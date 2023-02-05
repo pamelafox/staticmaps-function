32b4b77fd91e9104c55c213b2b0a27015566f58d
Pamela Fox <pamela.fox@gmail.com>
2023-02-05 20:13:40 +0000

    static maps test

--- IconWriter/__init__.py
+++ IconWriter/__init__.py
@@ -3,30 +3,24 @@ import io
 import azure.functions as func
 from icon_writer import write_icon
 
-def get_param(req, param_name, default_value=None):
-    param_value = req.params.get(param_name)
-    if not param_value:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            param_value = req_body.get(param_name)
-    return param_value if param_value is not None else default_value
+import staticmaps
+
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
-    text = get_param(req, 'text')
-    size = int(get_param(req, 'size', 80))
-    bgcolor = get_param(req, 'bgcolor', 'black')
-    fontcolor = get_param(req, 'fontcolor', 'white')
-    if text:
-        img = write_icon(text, size=size, bgcolor=bgcolor, fontcolor=fontcolor)
-        img_byte_arr = io.BytesIO()
-        img.save(img_byte_arr, format='PNG')
-        img_byte_arr = img_byte_arr.getvalue()
-        return func.HttpResponse(img_byte_arr, mimetype='image/png')
-    else:
-        return func.HttpResponse(
-             "Text must be specified",
-             status_code=400
-        )
+    context = staticmaps.Context()
+    context.set_tile_provider(staticmaps.tile_provider_StamenToner)
+
+    frankfurt = staticmaps.create_latlng(50.110644, 8.682092)
+    newyork = staticmaps.create_latlng(40.712728, -74.006015)
+
+    context.add_object(staticmaps.Line([frankfurt, newyork], color=staticmaps.BLUE, width=4))
+    context.add_object(staticmaps.Marker(frankfurt, color=staticmaps.GREEN, size=12))
+    context.add_object(staticmaps.Marker(newyork, color=staticmaps.RED, size=12))
+
+    # render non-anti-aliased png
+    image_pil = context.render_pillow(800, 500)
+
+    img_byte_arr = io.BytesIO()
+    image_pil.save(img_byte_arr, format='PNG')
+    img_byte_arr = img_byte_arr.getvalue()
+    return func.HttpResponse(img_byte_arr, mimetype='image/png')
