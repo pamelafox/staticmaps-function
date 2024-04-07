efabe4827274aa5fd40e3c070b507ae524bd3d43
Pamela Fox <pamela.fox@gmail.com>
2024-04-07 14:17:42 +0000

    Fix FastAPI otel issue

--- function/__init__.py
+++ function/__init__.py
@@ -1,20 +1,3 @@
-import os
-
-from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
-from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
-from opentelemetry.sdk.resources import SERVICE_NAME, Resource
-from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace.export import BatchSpanProcessor
-
 from .fastapi_app import create_app
 
 fastapi_app = create_app()
-
-
-@fastapi_app.on_event("startup")
-async def startup_event():
-    if conn_str := os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
-        exporter = AzureMonitorTraceExporter.from_connection_string(conn_str)
-        tracer = TracerProvider(resource=Resource({SERVICE_NAME: "api"}))
-        tracer.add_span_processor(BatchSpanProcessor(exporter))
-        FastAPIInstrumentor.instrument_app(fastapi_app, tracer_provider=tracer)
--- function/fastapi_app.py
+++ function/fastapi_app.py
@@ -1,9 +1,23 @@
+import os
+
 import fastapi
+from azure.monitor.opentelemetry.exporter import AzureMonitorTraceExporter
+from opentelemetry.instrumentation.fastapi import FastAPIInstrumentor
+from opentelemetry.sdk.resources import SERVICE_NAME, Resource
+from opentelemetry.sdk.trace import TracerProvider
+from opentelemetry.sdk.trace.export import BatchSpanProcessor
 
 from . import fastapi_routes
 
 
 def create_app():
     app = fastapi.FastAPI(docs_url="/")
+    if conn_str := os.getenv("APPLICATIONINSIGHTS_CONNECTION_STRING"):
+        exporter = AzureMonitorTraceExporter.from_connection_string(conn_str)
+        tracer = TracerProvider(resource=Resource({SERVICE_NAME: "api"}))
+        tracer.add_span_processor(BatchSpanProcessor(exporter))
+        FastAPIInstrumentor.instrument_app(app, tracer_provider=tracer)
+
     app.include_router(fastapi_routes.router)
+
     return app
